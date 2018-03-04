fe7269f71df65355231c96534ba995401fe00a62
dezidaisy <32540506+dezidaisy@users.noreply.github.com>
2018-03-04 17:21:08 +0000

    conversion

---
+++ Conversion/Conversion/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Conversion.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+namespace Conversion
+{
+    public class HomeController : Controller
+    {
+        public static ConversionUnit CUnit = new ConversionUnit();
+        public IActionResult Index()
+        {
+
+
+            return View(CUnit);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("Conversion")]
+        public IActionResult conversion(IFormCollection formCollection)
+        {
+            double number;
+            if (formCollection["UnitType"] == "Metric")
+            {
+
+                Double.TryParse(formCollection["fahrenheit"], out number);
+                CUnit.fahrenheit = number;
+                CUnit.tcelsius();
+
+                Double.TryParse(formCollection["feet"], out number);
+                CUnit.feet = number;
+                CUnit.lmeters();
+
+                Double.TryParse(formCollection["pounds"], out number);
+                CUnit.pounds = number;
+                CUnit.wkg();
+            }
+            else
+            {
+                Double.TryParse(formCollection["celsius"], out number);
+                CUnit.celsius = number;
+                CUnit.tfahrenheit();
+
+                Double.TryParse(formCollection["meters"], out number);
+                CUnit.meters = number;
+                CUnit.lfeet();
+
+                Double.TryParse(formCollection["kg"], out number);
+                CUnit.kg = number;
+                CUnit.wpounds();
+
+            }
+            //formCollection["celsius"] = "abc";
+            return View("Index");
+        }
+    }
+}
---
+++ Conversion/Conversion/Models/ConversionUnit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    public class ConversionUnit
+    {
+        public double celsius { get; set; }
+        public double fahrenheit { get; set; }
+        public double meters { get; set; }
+        public double feet { get; set; }
+        public double kg { get; set; }
+        public double pounds { get; set; }
+        public void tcelsius()
+        {
+            celsius = (fahrenheit - 32)/1.8;
+           
+        }
+        public void tfahrenheit()
+        {
+            fahrenheit = (celsius * 1.8)+32;
+            
+        }
+        public void lmeters()
+        {
+            meters = feet * 0.3048;
+  
+        }
+        public void lfeet()
+        {
+            feet = meters / 0.3048;
+            
+        }
+        public void wkg()
+        {
+            kg = pounds * 0.45359237;
+            
+        }
+        public void wpounds()
+        {
+            pounds = kg / 0.45359237;
+            
+        }
+
+
+
+    }
+}
